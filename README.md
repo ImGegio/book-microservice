0940a0e773efbcfea1191670bee8c48dfb65f202
LorenzoElia95 <lorenzo.elia.pro@gmail.com>
2024-03-29 16:51:31 +0100

    Implementing BookMapper.java

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ online bookstore simple project
 2 - Si potrebbe considerare la creazione di schema dedicati ad ogni microservizio di modo da non interferire negli sviluppi di altri colleghi
 
 3 - Considerare l'utilizzo di controlli di validazione offerti direttamente da springframework
+
+4 - In alcuni metodi dei controller, si può evitare di specificare path diversi in quanto i method sono già differenti fra loro e quindi distinguibili a livello di chiamata http
+
+5 - Mapper creato con BeanUtils, per esigenze piu' specifiche si consideri l'implementazione di Mapstruct
+
---
+++ src/main/java/com/book/microservice/bookmicroservice/mapper/BookMapper.java
@@ -0,0 +1,55 @@
+package com.book.microservice.bookmicroservice.mapper;
+
+import com.book.microservice.bookmicroservice.entity.Book;
+import com.book.microservice.bookmicroservice.model.BookDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class BookMapper {
+
+    /**
+     * @return a Book from a BookDto
+     */
+    public Book toEntity(BookDto dto) {
+        Book entity = new Book();
+        BeanUtils.copyProperties(dto, entity);
+        return entity;
+    }
+
+    /**
+     * @return a BookDto from a Book
+     */
+    public BookDto toDto(Book entity) {
+        BookDto dto = new BookDto();
+
+        BeanUtils.copyProperties(entity, dto);
+        return dto;
+    }
+
+    /**
+     * @return a list of Book from a BookDtoList
+     */
+    public List<Book> toEntity(List<BookDto> dto) {
+        List<Book> entity = new ArrayList<>();
+        for (BookDto current : dto) {
+            entity.add(toEntity(current)) ;
+        }
+        return entity;
+    }
+
+    /**
+     * @return a BookDtoList from a list of Book
+     */
+    public List<BookDto> toDto(List<Book> entity) {
+        List<BookDto> dto = new ArrayList<>();
+        for (Book current : entity) {
+            dto.add(toDto(current)) ;
+        }
+        return dto;
+    }
+}
