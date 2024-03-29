614adf55e1601288c9f02c4ba031f356c701dbaa
LorenzoElia95 <lorenzo.elia.pro@gmail.com>
2024-03-29 17:21:33 +0100

    Connected all layers

--- src/main/java/com/book/microservice/bookmicroservice/controller/impl/BookControllerImpl.java
+++ src/main/java/com/book/microservice/bookmicroservice/controller/impl/BookControllerImpl.java
@@ -2,7 +2,11 @@ package com.book.microservice.bookmicroservice.controller.impl;
 
 import com.book.microservice.bookmicroservice.controller.BookController;
 import com.book.microservice.bookmicroservice.model.BookDto;
+import com.book.microservice.bookmicroservice.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -11,27 +15,32 @@ import java.util.List;
 @RestController("/book")
 public class BookControllerImpl implements BookController {
 
+    @Autowired
+    private BookService bookService;
+
     @Override
     public ResponseEntity<List<BookDto>> getAllBooks() {
-        return null;
+        return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<BookDto> getBookById(Long bookId) {
-        return null;
+        return new ResponseEntity<>(bookService.getBookById(bookId), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<BookDto> insertBook(BookDto bookDto) {
-        return null;
+        return new ResponseEntity<>(bookService.insertBook(bookDto), HttpStatus.CREATED);
     }
 
     @Override
     public ResponseEntity<BookDto> updateBook(BookDto bookDto) {
-        return null;
+        return new ResponseEntity<>(bookService.updateBook(bookDto), HttpStatus.OK);
     }
 
     @Override
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBook(Long bookId) {
+        bookService.deleteBook(bookId);
     }
 }
